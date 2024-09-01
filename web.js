6ce23670be9391995840688aeee135af205726e7
aakrfja <94963460+aakrfja@users.noreply.github.com>
2024-09-02 01:03:02 +0700

    Add files via upload

--- web.js
+++ web.js
@@ -34,10 +34,16 @@ function showPopup() {
     }
 }
 
+
 function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('hidden');
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
 }
+
 
 document.getElementById("home").addEventListener("click", function(event) {
     event.preventDefault();
