1e44a79c46f5b61966a84be5bc8d63510959e6e5
aakrfja <94963460+aakrfja@users.noreply.github.com>
2024-09-25 16:06:47 +0700

    Add files via upload

--- smile.js
+++ smile.js
@@ -2,31 +2,41 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 const btnGroup = document.querySelector(".btn-group");
 
+// Function to move the 'No' button to a random position within the btn-group container
+function moveNoButtonRandomly() {
+    const maxX = btnGroup.clientWidth - noBtn.offsetWidth;
+    const maxY = btnGroup.clientHeight - noBtn.offsetHeight;
+
+    // Generate a random position within the btn-group container
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    // Apply the new random position to the 'No' button
+    noBtn.style.position = "absolute";
+    noBtn.style.left = `${randomX}px`;
+    noBtn.style.top = `${randomY}px`;
+}
+
+// Add event listener for 'Yes' button click
 yesBtn.addEventListener("click", () => {
     const question = document.querySelector(".question");
     const gif = document.querySelector(".gif");
 
-    question.innerHTML = "I love you too!🫶😘";
+    question.innerHTML = "I love you too! 😘🫶";
     gif.src = "https://i.pinimg.com/originals/55/3d/42/553d42bea9b10e0662a05aa8726fc7f4.gif";
     
-    yesBtn.style.display = "none";
+    // Hide the 'No' button after 'Yes' is clicked
     noBtn.style.display = "none";
 });
 
-// Function to move the 'No' button to a random position within the btn-group container
-
+// Add event listener to move 'No' button when the cursor hovers over it
 noBtn.addEventListener("mouseover", () => {
     setTimeout(() => {
-        const maxX = btnGroup.clientWidth - noBtn.offsetWidth;
-        const maxY = btnGroup.clientHeight - noBtn.offsetHeight;
-
-        // Generate random position within the btn-group container
-        const randomX = Math.floor(Math.random() * maxX);
-        const randomY = Math.floor(Math.random() * maxY);
-
-        noBtn.style.position = "absolute";
-        noBtn.style.left = `${randomX}px`;
-        noBtn.style.top = `${randomY}px`;
-    }, 0.005); // Delay of 500 milliseconds (0.5 seconds)
+        moveNoButtonRandomly();
+    }, 500); // 500ms delay for movement
 });
 
+// Add event listener to move 'No' button when clicked
+noBtn.addEventListener("click", () => {
+    moveNoButtonRandomly();
+});
